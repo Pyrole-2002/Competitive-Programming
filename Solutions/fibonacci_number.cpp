f718afdd85e3e4abde4d30ac3846b5601afbcdd4
Aryan Bansal <78760369+Pyrole-2002@users.noreply.github.com>
2023-12-21 15:33:30 +0530

    Create fibonacci_number.cpp

---
+++ Solutions/fibonacci_number.cpp
@@ -0,0 +1,122 @@
+/*
+https://codeforces.com/problemset/problem/193/E
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <deque>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+using ll = int64_t;
+#define INF (int)((1ll << 31) - 1)
+#define REP(i, start, end) for (ll i = start; (i) < (ll)(end); (i)++)
+#define MOD (ll)(1e13)
+
+ll mod(ll n, ll m)
+{
+    return (((n % m) + m) % m);
+}
+
+ll binmul(ll a, ll b, ll m)
+{
+    a = mod(a, m);
+    ll res = 0;
+    while (b)
+    {
+        if (b & 1)
+        {
+            res = mod(res + a, m);
+        }
+        a = mod(a + a, m);
+        b >>= 1;
+    }
+    return res;
+}
+
+void matmul(ll res[2][2], ll a[2][2], ll b[2][2])
+{
+    REP(i, 0, 2)
+    {
+        REP(j, 0, 2)
+        {
+            ll sum = 0;
+            REP(k, 0, 2)
+            {
+                sum += binmul(a[i][k], b[k][j], MOD);
+            }
+            res[i][j] = mod(sum, MOD);
+        }
+    }
+}
+
+ll fib(ll n)
+{
+    ll base[2][2] = {
+        {1, 1},
+        {1, 0}
+    };
+
+    ll result[2][2] = {
+        {1, 0},
+        {0, 1}
+    };
+    while(n)
+		{
+        if (n & 1)
+        {
+            ll tmp[2][2];
+            REP(i, 0, 2)
+            {
+                REP(j, 0, 2)
+                {
+                    tmp[i][j] = result[i][j];
+                }
+            }
+            matmul(result, base, tmp);
+        }
+        ll tmp[2][2];
+        REP(i, 0, 2)
+        {
+            REP(j, 0, 2)
+            {
+                tmp[i][j] = base[i][j];
+            }
+        }
+        matmul(base, tmp, tmp);
+        n >>= 1;
+    }
+    return mod(result[0][1], MOD);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll period = 15e12;
+
+    ll f;
+    cin >> f;
+
+    REP(i, 0, period)
+    {
+        if (fib(i) == f)
+        {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << -1 << endl;
+
+    return 0;
+}
